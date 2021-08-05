a348745f3ee6cfdb1e2e8afab5f97d20aca2e117
kenneth gabriel <31022243+keosariel@users.noreply.github.com>
2021-08-05 05:15:10 -0700

    Delete app directory

--- app/v0/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.v0.auth import auth
-from app.v0.users import users
-from app.v0.links  import links
-# from app.v0.uploads import uploads
--- app/v0/links/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from app.v0.links.views import LinkView
-
-links = Blueprint("links", __name__)
-link_view = LinkView.as_view('link_view')
-
-links.add_url_rule("/v0/links/<string:public_id>", view_func=link_view, methods=["GET", "DELETE", "PUT"])
-links.add_url_rule("/v0/links", view_func=link_view, methods=["POST"])
